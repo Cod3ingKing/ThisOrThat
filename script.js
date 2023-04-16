0f06977907fcc35f9a548b36bb2247cb465ffefb
Carney Burns <crb565@gmail.com>
2023-04-16 17:50:32 -0400

    commit 3

--- script.js
+++ script.js
@@ -1,61 +1,85 @@
 
-
-const questionElement = document.getElementById("question");
 const option1Element = document.getElementById("option1");
 const option2Element = document.getElementById("option2");
+const leaderboardElement = document.getElementById("leaderboard");
 
 const questions = [
   {
-    //question: "Which do you prefer, coffee or tea?",
     option1: {
       text: "Coffee",
-      image: "coffee.jpg"
+      image: "coffee.jpg",
+      clickCount: 0,
     },
     option2: {
       text: "Tea",
-      image: "tea.jpg"
-    }
+      image: "tea.jpg",
+      clickCount: 0,
+    },
   },
   {
-    //question: "Which do you prefer, beach or mountains?",
     option1: {
       text: "Beach",
-      image: "beach.jpg"
+      image: "beach.jpg",
+      clickCount: 0,
     },
     option2: {
       text: "Mountains",
-      image: "mountains.jpg"
-    }
-  }
+      image: "mountains.jpg",
+      clickCount: 0,
+    },
+  },
 ];
 
 let currentQuestionIndex = 0;
 
 function showQuestion() {
   const currentQuestion = questions[currentQuestionIndex];
-  //questionElement.innerText = currentQuestion.question;
   option1Element.src = currentQuestion.option1.image;
   option2Element.src = currentQuestion.option2.image;
 }
 
 function selectOption(optionIndex) {
-  // Do something with the selected option
-  console.log(`Option ${optionIndex + 1} selected!`);
+  const currentQuestion = questions[currentQuestionIndex];
+  const selectedOption =
+    optionIndex === 0 ? currentQuestion.option1 : currentQuestion.option2;
 
-  // Move to the next question
+  selectedOption.clickCount++;
+
   currentQuestionIndex++;
   if (currentQuestionIndex >= questions.length) {
-    // If we've reached the end of the questions, start over
     currentQuestionIndex = 0;
+    showLeaderboard();
+    
   }
 
-  // Show the next question
   showQuestion();
 }
 
-// Add click event listeners to the option boxes
+function showLeaderboard() {
+  leaderboardElement.innerHTML = "";
+  const sortedOptions = getSortedOptions();
+
+  sortedOptions.forEach((option) => {
+    const optionElement = document.createElement("div");
+    optionElement.classList.add("leaderboard-option");
+    optionElement.innerHTML = `
+      <img class="leaderboard-image" src="${option.image}">
+      <div class="leaderboard-count">${option.clickCount} clicks</div>
+    `;
+    leaderboardElement.appendChild(optionElement);
+  });
+}
+
+function getSortedOptions() {
+  const allOptions = [];
+  questions.forEach((question) => {
+    allOptions.push(question.option1, question.option2);
+  });
+
+  return allOptions.sort((a, b) => b.clickCount - a.clickCount);
+}
+
 option1Element.addEventListener("click", () => selectOption(0));
 option2Element.addEventListener("click", () => selectOption(1));
 
-// Show the first question
 showQuestion();
